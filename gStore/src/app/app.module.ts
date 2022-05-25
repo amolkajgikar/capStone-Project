77db56db9ca4064539396e5efd09cd4a9467a109
Amol Kajgikar <amolkajgikar@gmail.com>
2022-05-25 16:04:24 +0400

    3rd commit

--- gStore/src/app/app.module.ts
+++ gStore/src/app/app.module.ts
@@ -1,14 +1,19 @@
+
+
+import { AuthModule } from './auth/auth.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
